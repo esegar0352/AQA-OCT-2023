d13ca79aaf2b451a285edd5fec14a840c575b5b6
KaterynaBila <t466745@deluxe.com>
2023-10-07 21:42:15 +0300

    DONE: write catch for null pointer exception - done. f57 DONE: write method that will cause NPE - done f.21

--- src/java/main/org/prog/exceptions/ExceptionsDemo.java
+++ src/java/main/org/prog/exceptions/ExceptionsDemo.java
@@ -6,25 +6,33 @@ import java.io.FileReader;
 
 public class ExceptionsDemo {
 
-    //TODO: write catch for null pointer exception
-    //TODO: write method that will cause NPE
+    //TODO: write catch for null pointer exception - done. f. 57
+    //TODO: write method that will cause NPE - done f.21
 
     public static void main(String[] args) {
-
-        equalsA("b");
-        equalsA("a");
-        equalsA("");
-        equalsA(null);
 //
+//        equalsA("b");
+//        equalsA("a");
+//        equalsA("");
+//        equalsA(null);
+
+        try {
+            causeNPE();
+        } catch (NullPointerException nullPointerException) {
+            System.out.println("Null Pointer exceptiom");
+        }  finally {
+            System.out.println("Null Pointer exception is catched");
+        }
 //        try {
 //            doSmth();
 //            doAnother();
-//        } catch (Throwable e) {
+//
+//       } catch (Throwable e) {
 //            System.out.println("This is another exception");
 //        } finally {
-//            System.out.println("this code is always executed");
-//        }
-//        System.out.println("test2");
+//           System.out.println("this code is always executed");
+//      }
+//      System.out.println("test2");
     }
 
     public static void doSmth() throws FileNotFoundException {
@@ -43,4 +51,12 @@ public class ExceptionsDemo {
     public static void equalsA(String value) {
         System.out.println("a".equals(value));
     }
-}
+
+    public static void causeNPE()  { //Method written  by Kateryna Bila - Method causes Null Pointer Exception
+      String a = null;
+        System.out.println(a.length());
+    }
+
+
+
+    }
